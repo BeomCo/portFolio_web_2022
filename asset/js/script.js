9c7e07e6cb175c0ef86b5f5652810c1b47f8e93e
Beomseok OH <ilsny7@me.com>
2022-02-21 06:12:14 +0900

    20220221

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,3 +1,11 @@
+const main = document.querySelector('#main');
+const introduce_1 = document.querySelector('#introduce_1');
+const introduce_2 = document.querySelector('#introduce_2');
+const portfolio_1 = document.querySelector('#portfolio_1');
+const portfolio_2 = document.querySelector('#portfolio_2');
+const contact = document.querySelector('#contact');
+
+
 const wrapLineInsert = document.querySelectorAll('.wrapLine');
 const wrapLineHtml = `
 	<div class="leftLine"></div>
@@ -9,6 +17,8 @@ for(wli of wrapLineInsert){
 	wli.innerHTML = wrapLineHtml;
 }
 
+
+// 루프
 let loopInt = 0;
 const loopDivFrame = document.querySelectorAll('.loopDivFrame');
 // console.log(loopDivFrame);
@@ -27,6 +37,38 @@ for(let i of loopDivFrame){
 
 }
 
+const portFolioloopDivFrame = document.querySelectorAll('.portFolioLoopDivFrame');
+for(let i of portFolioloopDivFrame){
+	setInterval(function(){
+	
+		if(loopInt > -50){
+			i.style.transform = `translateX(${loopInt}%)`;
+			loopInt -= 0.005;
+		}else if(loopInt = -50){
+			i.style.transition = ``;
+			loopInt = 0;
+		}
+	},0)
+
+}
+
+let loopDivContact = 0;
+
+const contactLoopDivFrame = document.querySelectorAll('.contactLoopDivFrame');
+for(let i of contactLoopDivFrame){
+	setInterval(function(){
+	
+		if(loopDivContact > -30){
+			i.style.transform = `translateX(${loopDivContact}%)`;
+			loopDivContact -= 0.01;
+		}else if(loopDivContact = -30){
+			i.style.transition = ``;
+			loopDivContact = 0;
+		}
+	},0)
+
+}
+
 
 document.addEventListener("mousemove" , parallax);
 function parallax(e){
@@ -34,8 +76,8 @@ function parallax(e){
 
 		const speed = parallax.getAttribute('data-speed');
 
-		const x = (window.innerWidth/45 - e.pageX*speed/50);
-		const y = (window.innerHeight/10 - e.pageY*speed/50);
+		const x = (introduce_1.offsetWidth/45 - e.pageX*speed/50);
+		const y = (introduce_1.offsetHeight/10 - e.pageY*speed/50);
 		parallax.style.transform = `translateY(${y}px) translateX(${x}px)`;
 		//translateX(${x}px)
 	})
@@ -48,16 +90,18 @@ for(let i of personaHSel){
 	i.addEventListener('mouseover', function(){
 		// console.log(i);
 		i.style.fontSize = '5em';
-		i.style.color = '#FFDD00';
-		document.querySelector('#introduce_1').style.backgroundColor = 'rgba(45, 45, 45, 0.3)'
+		i.style.color = '#F2CD02';
+		document.querySelector('#introduce_1').style.backgroundColor = 'rgba(45, 45, 45, 0.9)'
 	}) 
 	i.addEventListener('mouseout', function(){
 		// console.log(i);
 		i.style.fontSize = '3.4em';
-		i.style.color = '#212121';
-		document.querySelector('#introduce_1').style.backgroundColor = '#E9E9E9'
+		i.style.color = '#2D2D2D';
+		document.querySelector('#introduce_1').style.backgroundColor = '#EBEEF7'
 	}) 
 }
+
+// const persona
 
 
 // 전체 세로 값
@@ -94,22 +138,22 @@ function scrollFnc(){
 // 	if(scroll >= heightSection_3){
 // 		headerClassClear();
 // 		headerAtagCbFunc();
-// 		header.classList.add('bcw');
+// 		header.classList.add('bgw');
 // 	}else if(scroll >= heightSection_4 && scroll <= heightSection_5){
 // 		headerClassClear();
 // 		headerAtagCwFunc();
-// 		header.classList.add('bcb');
+// 		header.classList.add('bgb');
 // 	}else if(scroll >= heightSection_5 && scroll <= heightSection_6){
 // 		headerClassClear();
 // 		headerAtagCbFunc();
-// 		header.classList.add('bcw');
+// 		header.classList.add('bgw');
 // 	}else if(scroll >= heightSection_6){
 // 		headerClassClear();
-// 		header.classList.add('bcb');
+// 		header.classList.add('bgb');
 // 	}else{
 // 		headerClassClear();
 // 		headerAtagCwFunc();
-// 		header.classList.add('bcb');
+// 		header.classList.add('bgb');
 // 	}
 // }
 
@@ -172,12 +216,7 @@ let heightSection_6 = heightN * 5 - headerHeight;
 
 console.log(heightN);
 
-const main = document.querySelector('#main');
-const introduce_1 = document.querySelector('#introduce_1');
-const introduce_2 = document.querySelector('#introduce_2');
-const portfolio_1 = document.querySelector('#portfolio_1');
-const portfolio_2 = document.querySelector('#portfolio_2');
-const contact = document.querySelector('#contact');
+
 
 //	스크롤 이벤트
 addEventListener('scroll', function(){
@@ -189,26 +228,26 @@ addEventListener('scroll', function(){
 	if(scroll <= heightSection_3){
 		headerClassClear();
 		headerAtagCwFunc();
-		header.classList.add('bcb');
-		// introduce_1.classList.add('bcb')
+		header.classList.add('bgb');
+		// introduce_1.classList.add('bgb')
 
 	}else if(scroll >= heightSection_3){
 		headerClassClear();
 		headerAtagCbFunc();
-		header.classList.add('bcw');
+		header.classList.add('bgw');
 
 		if(scroll >= heightSection_4){
 			headerClassClear();
 			headerAtagCwFunc();
-			header.classList.add('bcb');
+			header.classList.add('bgb');
 			if(scroll >= heightSection_5){
 				headerClassClear();
 				headerAtagCbFunc();
-				header.classList.add('bcw');
+				header.classList.add('bgw');
 				if(scroll >= heightSection_6){
 					headerClassClear();
 					headerAtagCwFunc();
-					header.classList.add('bcb');
+					header.classList.add('bgb');
 				}
 			}
 		}
@@ -217,6 +256,73 @@ addEventListener('scroll', function(){
 	// console.log(scroll >= heightSection_4 && scroll <= heightSection_5);
 });
 
+
+const portFolioTypeFixed = document.querySelector('.portFolioType.fixed');
+const portFolioTypeResponsive = document.querySelector('.portFolioType.responsive');
+const portFolioTypeCode = document.querySelector('.portFolioType.code');
+
+const portFolioTypeH = document.querySelector('.portFolioTypeH h3');
+const portFolioTypeHDefault = document.querySelector('.portFolioTypeH .default');
+const portFolioTypeHFixed = document.querySelector('.portFolioTypeH .fixed');
+const portFolioTypeHResponsive = document.querySelector('.portFolioTypeH .responsive');
+const portFolioTypeHCode = document.querySelector('.portFolioTypeH .code');
+const returnDefault = document.querySelector('.returnDefault');
+
+const portFolioMiddleFixed = document.querySelector('.portFolioMiddleFixed')
+const portFolioMiddleResponsive = document.querySelector('.portFolioMiddleResponsive')
+const portFolioMiddleCode = document.querySelector('.portFolioMiddleCode')
+const portFolioMiddleDefault = document.querySelector('.portFolioMiddleDefault')
+
+
+function portFolioMiddleDeault(){
+	portFolioMiddleFixed.classList.remove('active');
+	portFolioMiddleResponsive.classList.remove('active');
+	portFolioMiddleCode.classList.remove('active')
+	returnDefault.classList.remove('active');
+	portFolioMiddleDefault.classList.add('active');
+}
+
+function portFolioMiddleClear(){
+	portFolioMiddleFixed.classList.remove('active');
+	portFolioMiddleResponsive.classList.remove('active');
+	portFolioMiddleCode.classList.remove('active');
+	portFolioMiddleDefault.classList.remove('active');
+	portFolioTypeHFixed.classList.remove('active');
+	portFolioTypeHResponsive.classList.remove('active');
+	portFolioTypeHCode.classList.remove('active');
+	portFolioTypeH.classList.remove('active');
+
+}
+// console.log(portFolioTypeResponsive);
+
+returnDefault.addEventListener('click', function(){
+	portFolioMiddleDeault();
+	portFolioMiddleClear();
+	portFolioMiddleDefault.classList.add('active')
+	portFolioTypeH.classList.add('active');
+})
+
+portFolioTypeFixed.addEventListener('click', function(){
+	portFolioMiddleClear();
+	portFolioMiddleFixed.classList.add('active');
+	
+	portFolioTypeHFixed.classList.add('active');
+	returnDefault.classList.add('active');
+})
+portFolioTypeResponsive.addEventListener('click', function(){
+	portFolioMiddleClear();
+	portFolioMiddleResponsive.classList.add('active');
+	portFolioTypeHResponsive.classList.add('active');
+	returnDefault.classList.add('active');
+})
+portFolioTypeCode.addEventListener('click', function(){
+	portFolioMiddleClear();
+	portFolioMiddleCode.classList.add('active');
+	portFolioTypeHCode.classList.add('active');
+	returnDefault.classList.add('active');
+})
+
+
 // import Swiper from '../js/swiper-bundle.min.js';
 
 
@@ -224,7 +330,7 @@ const swiper = new Swiper(".swiper", {
     // autoplay: true,
     speed: 500,
     spaceBetween: 2000,
-    loop: true,
+    // loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -239,5 +345,18 @@ const swiper = new Swiper(".swiper", {
 		translate: ["120%", 0, -500],
 		},
 	},
+
+});
+const swiperFolio = new Swiper(".swiperFolio", {
+    slidesPerView: 3,
+        // spaceBetween: 200,
+        slidesPerGroup: 3,
+        // loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+       
 
 });
